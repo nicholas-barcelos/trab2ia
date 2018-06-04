400f236c910d4af339cdd767bbed6487c7f7a64d
Marco Bonetti <marco_bonetti@uol.com.br>
2018-06-03 21:52:03 -0300

    Base alterada com dez perguntas

--- base.pl
+++ base.pl
@@ -19,65 +19,87 @@ rio(murray, australia).
 pontomaisalto(puncack_jaya, papua_nova_guine).
 pontomaisbaixo(lago_eyre, australia).
 
-/* Idiomas */
-idioma(bislama).
-idioma(castelhano).
-idioma(chamorro).
-idioma(fidjiano).
-idioma(frances).
-idioma(gilbertes).
-idioma(havaiano).
-idioma(indonesio).
-idioma(ingles).
-idioma(lingua_de_sinais_da_nova_zelandia).
-idioma(maori).
-idioma(marquesano).
-idioma(marshales).
-idioma(nauruano).
-idioma(palauense).
-idioma(rapanui).
-idioma(taitiano).
-idioma(toquelauano).
+/* Idiomas(pais, idioma */
+idioma(vanuatu,bislama).
+idioma(australia,ingles).
+idioma(guam,chamorro).
+idioma(fiji,fidjiano).
+idioma(kiribati,gilbertes).
+idioma(indonesia,indonesio).
+idioma(indonesia,ingles).
+idioma(nova_zelandia,lingua_de_sinais_da_nova_zelandia).
+idioma(nova_zelandia,maori).
+idioma(nova_zelandia,ingles).
+idioma(ilhas_marshall,marshales).
+idioma(nauru,nauruano).
+idioma(palau,palauense).
+idioma(polinesia_francesa,taitiano).
+idioma(tuvalu,toquelauano).
 
-/* Regioes */
-regiao(australasia).
-regiao(melanesia).
-regiao(micronesia).
-regiao(polinesia).
+/* Regioes (pais, regiao)*/
+regiao(australia,australasia).
+regiao(ilha_do_natal,australasia).
+regiao(ilhas_cocos,australasia).
+regiao(nova_zelandia,australasia).
+regiao(ilha_norfolk,australasia).
+regiao(timor_leste,melanesia).
+regiao(fiji,melanesia).
+regiao(indonesia, melanesia).
+regiao(nova_caledonia, melanesia).
+regiao(papua_nova_guine,melanesia).
+regiao(ilhas_salomao, melanesia).
+regiao(vanuatu,melanesia).
+regiao(estados_federados_da_micronesia, micronesia).
+regiao(guam, micronesia).
+regiao(kiribati,micronesia).
+regiao(ilhas_marshall, micronesia).
+regiao(nauru,micronesia).
+regiao(marianas_setentrionais, micronesia).
+regiao(palau, micronesia).
+regiao(samoa_americana, polinesia).
+regiao(ilhas_cook, polinesia).
+regiao(polinesia_francesa, polinesia).
+regiao(niue, polinesia).
+regiao(pitcairn, polinesia).
+regiao(samoa, polinesia).
+regiao(toquelau, polinesia).
+regiao(tonga, polinesia).
+regiao(tuvalu, polinesia).
+regiao(wallis_e_futuna, polinesia).
 
-/* País (nome, regiao, area em km^2, populacao, densidade populacional, capital)*/
-pais(australia,australasia, 7686850, 21050000, 2.5, cambera).
-pais(ilha_do_natal,australasia, 135, 1493, 3.5, flying_fish_cove).
-pais(ilhas_cocos,australasia, 14, 632, 45.1, west_island).
-pais(nova_zelandia,australasia, 268680, 4108037, 14.5, wellington).
-pais(ilha_norfolk,australasia, 35,  1866, 53.3, kingston).
+/* País (nome, area em km^2, populacao, densidade populacional, capital)*/
+pais(australia,7686850, 21050000, 2.5, cambera).
+pais(ilha_do_natal, 135, 1493, 3.5, flying_fish_cove).
+pais(ilhas_cocos, 14, 632, 45.1, west_island).
+pais(nova_zelandia, 268680, 4108037, 14.5, wellington).
+pais(ilha_norfolk, 35,  1866, 53.3, kingston).
 
-pais(timor_leste,melanesia, 15007, 947000, 8.0, dili).
-pais(fiji,melanesia, 18270, 856346, 46.9, suva).
-pais(indonesia, melanesia, 499852, 4211532, 8.4, jacarta).
-pais(nova_caledonia, melanesia, 19060, 207858, 10.9, noumea).
-pais(papua_nova_guine,melanesia, 462840, 5172033, 11.2, port_moresby).
-pais(ilhas_salomao, melanesia, 28450, 494786, 17.4, honiara).
-pais(vanuatu,melanesia, 12200, 196178, 16.1, port_vila).
+pais(timor_leste, 15007, 947000, 8.0, dili).
+pais(fiji, 18270, 856346, 46.9, suva).
+pais(indonesia, 499852, 4211532, 8.4, jacarta).
+pais(nova_caledonia, 19060, 207858, 10.9, noumea).
+pais(papua_nova_guine, 462840, 5172033, 11.2, port_moresby).
+pais(ilhas_salomao, 28450, 494786, 17.4, honiara).
+pais(vanuatu, 12200, 196178, 16.1, port_vila).
 
-pais(estados_federados_da_micronesia, micronesia, 702, 135869, 193.5, palikir).
-pais(guam, micronesia, 549, 160796, 292.9, hagatna).
-pais(kiribati,micronesia, 811,96335, 118.8, bairiki).
-pais(ilhas_marshall, micronesia, 181,73630, 406.8, majuro).
-pais(nauru,micronesia, 21, 12329, 587.1, distrito_capital_de_yaren).
-pais(marianas_setentrionais, micronesia, 477, 77311, 162.1, saipan).
-pais(palau, micronesia, 458,19409, 42.4, melekeok).
+pais(estados_federados_da_micronesia, 702, 135869, 193.5, palikir).
+pais(guam, 549, 160796, 292.9, hagatna).
+pais(kiribati, 811,96335, 118.8, bairiki).
+pais(ilhas_marshall, 181,73630, 406.8, majuro).
+pais(nauru, 21, 12329, 587.1, distrito_capital_de_yaren).
+pais(marianas_setentrionais, 477, 77311, 162.1, saipan).
+pais(palau, 458,19409, 42.4, melekeok).
 
-pais(samoa_americana, polinesia, 199, 68688, 345.2, pago_pago).
-pais(ilhas_cook, polinesia, 240, 20811, 86.7, avarua).
-pais(polinesia_francesa, polinesia, 4167,257847,61.9, papeete).
-pais(niue, polinesia, 260, 2134, 8.2, alofi).
-pais(pitcairn, polinesia, 5, 47, 10, adamstown).
-pais(samoa, polinesia, 2944, 178631, 60.7, apia).
-pais(toquelau, polinesia, 10, 1431, 143.1, nao_possui).
-pais(tonga, polinesia, 748, 106137, 141.9, nuku_alofa).
-pais(tuvalu, polinesia, 26,11146, 428.7, funafuti).
-pais(wallis_e_futuna, polinesia, 274, 15585, 56.9, mata_utu).
+pais(samoa_americana, 199, 68688, 345.2, pago_pago).
+pais(ilhas_cook, 240, 20811, 86.7, avarua).
+pais(polinesia_francesa, 4167,257847,61.9, papeete).
+pais(niue, 260, 2134, 8.2, alofi).
+pais(pitcairn, 5, 47, 10, adamstown).
+pais(samoa, 2944, 178631, 60.7, apia).
+pais(toquelau, 10, 1431, 143.1, nao_possui).
+pais(tonga, 748, 106137, 141.9, nuku_alofa).
+pais(tuvalu, 26,11146, 428.7, funafuti).
+pais(wallis_e_futuna, 274, 15585, 56.9, mata_utu).
 
 /* zonas climáticas */
 zonaclima(tropical,norte).
@@ -122,14 +144,14 @@ fuso(vanuatu,"utc+11:00").
 fuso(wallis_e_futuna,"utc+12:00").
 
 /* cidades mais populosas(ranking, nome, pais, populacao) */
-cidadepopulosa(1, "Sydney",  australia, 3749902).
+cidadepopulosa(1, "Sydney",  australia, 3749902).
 cidadepopulosa(2, "Melbourne", australia, 3551659).
 cidadepopulosa(3, "Brisbane", australia, 1478713).
 cidadepopulosa(4, "Macáçar", indonesia, 1256095).
 cidadepopulosa(5, "Perth", australia, 1317965).
 cidadepopulosa(6, "Adelaide", australia, 1064615).
 cidadepopulosa(7, "Manado", indonesia, 522390).
-cidadepopulosa(8, "Gold Coast - Tweed", australia, 504830).
+cidadepopulosa(8, "Gold Coast - Tweed", australia, 504830).
 cidadepopulosa(9, "Auckland", nova_zelandia, 421930).
 cidadepopulosa(10, "Ambon", indonesia, 416988).
 cidadepopulosa(11, "Manukau", nova_zelandia, 410158).
@@ -137,10 +159,53 @@ cidadepopulosa(12, "Christchurch", nova_zelandia, 377434).
 cidadepopulosa(13, "Honolulu", "EUA", 375134).
 cidadepopulosa(14, "Kupang", indonesia, 361666).
 cidadepopulosa(15, "Palu", indonesia, 348757).
-cidadepopulosa(16, "Camberra - Queanbeyan", australia, 334276).
+cidadepopulosa(16, "Camberra - Queanbeyan", australia, 334276).
 cidadepopulosa(17, "Mataram", indonesia, 329544).
 cidadepopulosa(18, "Kendari", indonesia, 307208).
 cidadepopulosa(19, "Port Moresby", papua_nova_guine, 307103).
 cidadepopulosa(20, "Central Coast", australia, 306025).
 
+q1:- classificacao(oceania,continente).
+q2:- composicao(oceania,grupos_de_ilhas).
+q3(CONTINENTE):- classificacao(CONTINENTE,continente),
+composicao(CONTINENTE,grupos_de_ilhas).
+q4(PAIS,IDIOMA):- idioma(PAIS,IDIOMA),
+rio(darling,PAIS),
+rio(murray,PAIS).
+q5(POSICAO,CIDADE,PAIS,CAPITAL):- pais(PAIS,_,_,_,CAPITAL),
+cidadepopulosa(POSICAO,CIDADE,PAIS,_).
+q6(IDIOMA,CIDADE,POSICAO):- idioma(PAIS,IDIOMA),
+cidadepopulosa(POSICAO,CIDADE,PAIS,_).
+q7(PAIS,CAPITAL,IDIOMA,REGIAO):- regiao(PAIS,REGIAO),
+pais(PAIS,_,_,_,CAPITAL),
+idioma(PAIS,IDIOMA).
+q8(L):- findall((REGIAO,PAIS,FUSO),(fuso(PAIS,FUSO),
+fuso(PAIS_UM,FUSO),
+regiao(PAIS,REGIAO),
+regiao(PAIS_UM,REGIAO),
+PAIS\==PAIS_UM),LR),list_to_set(LR,L).
+adiciona(X,L,[X|L]).
+q9(L):- findall((REGIAO,PAIS,IDIOMA),(idioma(PAIS,IDIOMA),
+idioma(PAIS_UM,IDIOMA),
+regiao(PAIS,REGIAO),
+regiao(PAIS_UM,REGIAO),
+PAIS\==PAIS_UM),LR),list_to_set(LR,L).
+q10(L,REGIAO):- findall((PAIS,IDIOMA),(idioma(PAIS,IDIOMA),
+regiao(PAIS,REGIAO)),LR),list_to_set(LR,L).
+adiciona(X,L,[X|L]).
 
+
+q1.
+q2.
+q3(oceania).
+q4(PAIS,IDIOMA).
+q5(POSICAO,CIDADE,PAIS,wellington).
+q5a(POSICAO,CIDADE,PAIS,cambera).
+q6(ingles,CIDADE,POSICAO).
+q6a(indonesio,CIDADE,POSICAO).
+q7(PAIS,CAPITAL,IDIOMA,polinesia).
+q7(PAIS,CAPITAL,IDIOMA,australasia).
+q8(L).
+q9(L).
+q10(L,indonesia).
+q10(L,australasia).
